9f9cd25e0e17b813a53f428a4a6c7af0551ca9c6
Admin <dbondarenko066@mail.ru>
2022-05-05 22:51:32 +0300

    AppConfih

---
+++ Discord Bot/AppConfig.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+
+namespace Discord_Bot;
+
+public static class AppConfig
+{
+    public static string Token => Environment.GetEnvironmentVariable("TOKEN") ?? string.Empty;
+
+    public static string ConnectionString;
+
+    static AppConfig()
+    {
+        ConnectionString = ConvertDatabaseUrlToConnectionString(Environment.GetEnvironmentVariable("DATABASE_URL") ?? string.Empty);
+    }
+
+    private static string ConvertDatabaseUrlToConnectionString(string databaseUrl)
+    {
+        var databaseUri = new Uri(databaseUrl);
+        var userInfo = databaseUri.UserInfo.Split(':');
+
+        var builder = new NpgsqlConnectionStringBuilder
+        {
+            Host = databaseUri.Host,
+            Port = databaseUri.Port,
+            Username = userInfo[0],
+            Password = userInfo[1],
+            Database = databaseUri.LocalPath.TrimStart('/')
+        };
+
+        return builder.ToString();
+    }
+}
